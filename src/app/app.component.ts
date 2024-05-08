115bc47309a0772cacdbb05c7cffd114eac4d16a
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-05-08 17:12:50 -0500

    Navbar animation update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,22 @@ export class AppComponent implements OnInit {
   openedNavItem!: HTMLElement;
   someString: string = "Hellooooo!";
 
-  openAccordeon(navItemId: string){
-    this.openedNavItem = document.getElementById(navItemId)!;
-    if (this.openedNavItem.classList.contains("opening")){
+  setAccordion(navItemId: string){
+    if (this.openedNavItem.id != navItemId){
       this.openedNavItem.classList.remove("opening");
       this.openedNavItem.classList.add("closing");
+      setTimeout(()=>{
+        this.openedNavItem = document.getElementById(navItemId)!;
+        this.openedNavItem.classList.add("opening");
+      }, 2000);
     } else {
-      this.openedNavItem.classList.add("opening");
-      this.openedNavItem.classList.remove("closing");
+      if (this.openedNavItem.classList.contains("opening")){
+        this.openedNavItem.classList.remove("opening");
+        this.openedNavItem.classList.add("closing");
+      } else {
+        this.openedNavItem.classList.add("opening");
+        this.openedNavItem.classList.remove("closing");
+      }
     }
     console.log(this.openedNavItem);
     console.log(this.openedNavItem.classList);
