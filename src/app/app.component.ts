ed48935a2ca710b527c18005cc63f95d528221f3
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-05-09 05:48:31 -0500

    Navbar update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'museums-front';
   openedNavItem!: HTMLElement;
-  someString: string = "Hellooooo!";
 
   setAccordion(navItemId: string){
+    console.log("OpenedNavItem at the begin: ", this.openedNavItem)
+    
+
+    if (this.openedNavItem == null){
+      this.openedNavItem = document.getElementById(navItemId)!;
+      console.log("First IF: openNavItem assigned from null: ", this.openedNavItem)
+    }
+    
     if (this.openedNavItem.id != navItemId){
+      console.log("First IF: different openNavItem item: ", this.openedNavItem)
+      // Closing opened navItem
       this.openedNavItem.classList.remove("opening");
       this.openedNavItem.classList.add("closing");
-      setTimeout(()=>{
-        this.openedNavItem = document.getElementById(navItemId)!;
-        this.openedNavItem.classList.add("opening");
-      }, 2000);
+      // Opening new navItem
+      this.openedNavItem = document.getElementById(navItemId)!;
+      this.openedNavItem.classList.remove("hide");
+      this.openedNavItem.classList.add("opening");
+      this.openedNavItem.classList.remove("closing");
     } else {
+      console.log("Second IF else: same openNavItem item: ", this.openedNavItem)
       if (this.openedNavItem.classList.contains("opening")){
         this.openedNavItem.classList.remove("opening");
         this.openedNavItem.classList.add("closing");
       } else {
+        this.openedNavItem.classList.remove("hide");
         this.openedNavItem.classList.add("opening");
         this.openedNavItem.classList.remove("closing");
       }
     }
-    console.log(this.openedNavItem);
     console.log(this.openedNavItem.classList);
   }
 
 
   ngOnInit() {
-    this.openedNavItem = document.getElementById("link-home")!;
+    //this.openedNavItem = document.getElementById("")!;
   }
 
 }
