c753d7ab82d98708610569dcaa4cdceed43a5309
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-05-09 09:27:28 -0500

    Update navbar transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,40 @@ export class AppComponent implements OnInit {
   openedNavItem!: HTMLElement;
 
   setAccordion(navItemId: string){
-    console.log("OpenedNavItem at the begin: ", this.openedNavItem)
-    
 
+    // Initializing the navItem
     if (this.openedNavItem == null){
       this.openedNavItem = document.getElementById(navItemId)!;
       console.log("First IF: openNavItem assigned from null: ", this.openedNavItem)
     }
     
+    // Case 1: navItem is different from the one opened already
     if (this.openedNavItem.id != navItemId){
       console.log("First IF: different openNavItem item: ", this.openedNavItem)
       // Closing opened navItem
       this.openedNavItem.classList.remove("opening");
       this.openedNavItem.classList.add("closing");
+      this.openedNavItem.classList.remove("open");
       // Opening new navItem
       this.openedNavItem = document.getElementById(navItemId)!;
       this.openedNavItem.classList.remove("hide");
       this.openedNavItem.classList.add("opening");
       this.openedNavItem.classList.remove("closing");
+      this.openedNavItem.classList.add("open");
     } else {
+    // Case 2: navItem is the same that is opened or was last opened
       console.log("Second IF else: same openNavItem item: ", this.openedNavItem)
       if (this.openedNavItem.classList.contains("opening")){
+        // Closing opened navItem
         this.openedNavItem.classList.remove("opening");
         this.openedNavItem.classList.add("closing");
+        this.openedNavItem.classList.remove("open");
       } else {
+        // Opening same navItem
         this.openedNavItem.classList.remove("hide");
         this.openedNavItem.classList.add("opening");
         this.openedNavItem.classList.remove("closing");
+        this.openedNavItem.classList.add("open");
       }
     }
     console.log(this.openedNavItem.classList);
