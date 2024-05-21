1b09216fdbfc3b78b5e1968462baa02a32d6e797
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-05-21 12:32:56 -0500

    Updates: Environment URL + Museum detail and Artwork detail styles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const baseUrl = 'https://museums-grupo13.herokuapp.com/api/';
+const baseUrl = 'http://127.0.0.1:3000/api/v1/';
 
 export const environment = {
  production: false,
