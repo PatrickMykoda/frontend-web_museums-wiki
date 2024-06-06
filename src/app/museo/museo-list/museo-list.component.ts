3e93f3033be7999c92762246061ce69f96d5e71f
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-06-06 17:23:05 -0500

    Update museums list

--- src/app/museo/museo-list/museo-list.component.ts
+++ src/app/museo/museo-list/museo-list.component.ts
@@ -13,17 +13,28 @@ export class MuseoListComponent implements OnInit {
   museos: Array<MuseoDetail> = [];
 
   selectedMuseo!: MuseoDetail;
-  selected = false;
 
   onSelected(museo: MuseoDetail): void {
-    this.selected = true;
+    if (this.selectedMuseo){
+      document.getElementById(this.selectedMuseo.id.toString())?.classList.remove('selected-list-item');
+    }
     this.selectedMuseo = museo;
+    document.getElementById(museo.id.toString())?.classList.add('selected-list-item');
   }
 
   getMuseos() {
     this.museoService.getMuseos().subscribe((museos) => {
       this.museos = museos;
+      this.onSelected(this.museos[0]);
+      setTimeout(() => { 
+        document.getElementById(this.museos[0].id.toString())?.classList.add('selected-list-item')
+      }, 50);
     });
+  }
+
+  hoverEffect(id: number) {
+    document.getElementById(id.toString())?.classList.add('list-item-hovered');
+    document.getElementById("bottom-"+id.toString())?.classList.add('item-bottom-hovered');
   }
 
   ngOnInit() {
