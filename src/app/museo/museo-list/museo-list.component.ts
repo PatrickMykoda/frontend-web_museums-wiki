1323789aaf3898d46a8f2e38bf9f204e152e64a6
PatrickMykoda <p.mykoda@uniandes.edu.co>
2024-06-08 10:59:53 -0500

    Update: Museums list with random artwork frames

--- src/app/museo/museo-list/museo-list.component.ts
+++ src/app/museo/museo-list/museo-list.component.ts
@@ -20,6 +20,7 @@ export class MuseoListComponent implements OnInit {
     }
     this.selectedMuseo = museo;
     document.getElementById(museo.id.toString())?.classList.add('selected-list-item');
+    this.setArtworkFrames();
   }
 
   getMuseos() {
@@ -35,6 +36,17 @@ export class MuseoListComponent implements OnInit {
   hoverEffect(id: number) {
     document.getElementById(id.toString())?.classList.add('list-item-hovered');
     document.getElementById("bottom-"+id.toString())?.classList.add('item-bottom-hovered');
+  }
+
+  setArtworkFrames(){
+    setTimeout(() => {
+      let artworkFrames = Array.from(document.getElementsByClassName('list-image-frame'));
+      artworkFrames.forEach(element => {
+        let randomNum = Math.floor(Math.random() * 9) + 1;
+        console.log("Random number: ",randomNum)
+        element.classList.add('frame'+randomNum.toString());
+      });
+    }, 50);
   }
 
   ngOnInit() {
